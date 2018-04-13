bedabaf6ce099b9400e5787e4d031c58754a58cd
Isaac <isaac@zfamily.org>
2018-04-13 01:53:07 -0600

    Added realtime options, incomplete socket implementation.

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 from key import Key
 from wav_manager import Wav_Manager
-
-
+from networking import MySocket as Socket
+#from networking import Host as host
+#from networking import Client as client
 
 if __name__ == "__main__":
+	'''
 	k = Key("\nParagraphs \n\
 	What this handout is about \n\
 	This handout will help you understand how paragraphs are formed, how to develop stronger paragraphs, and how to completely and clearly express your ideas. \n\
@@ -66,22 +68,33 @@ if __name__ == "__main__":
 	Here is a look at the completed “model” paragraph:\n\
 	Slave spirituals often had hidden double meanings. On one level, spirituals referenced heaven, Jesus, and the soul, but on another level, the songs spoke about slave resistance. For example, according to Frederick Douglass, the song “O Canaan, Sweet Canaan” spoke of slaves’ longing for heaven, but it also expressed their desire to escape to the North. Careful listeners heard this second meaning in the following lyrics: “I don’t expect to stay / Much longer here. / Run to Jesus, shun the danger. / I don’t expect to stay.” When slaves sang this song, they could have been speaking of their departure from this life and their arrival in heaven; however, they also could have been describing their plans to leave the South and run, not to Jesus, but to the North. Slaves even used songs like “Steal Away to Jesus (at midnight)” to announce to other slaves the time and place of secret, forbidden meetings. What whites heard as merely spiritual songs, slaves discerned as detailed messages. The hidden meanings in spirituals allowed slaves to sing what they could not say.')\n\
 	")
+	'''
 	
-	#k = Key("new message")
+	'''
+	k = Key("new message")
 	print ('Original:\n', k.getMessage())
 	k.saveToFile()
 	print ()
 	print ('Decoded from file:\n', k.decodeMessage(k.getFrequencies()))
 	print ()
-	print (sorted(k.getFrequencies())[:-4])
+	#print (sorted(k.getFrequencies()))
 	
 	w = Wav_Manager()
 	w.newWave(k.getFrequencies())
 	w.saveToFile()
+	#w.play()
+	#w.listen()
+	'''
 	
+	conn = Socket()
+	conn.connect('192.168.1.133', 80)
+	
+	
+	'''
 	plt.plot(w.getFile()[1][:150])
 	plt.show()
 	
 	plt.hist(sorted(k.getFrequencies())[:-4], 500)
 	plt.xlabel('frequencies')
 	plt.show()
+	'''
--- wav_manager.py
+++ wav_manager.py
@@ -1,16 +1,19 @@
 import numpy as np
+import pyaudio
 from scipy.io import wavfile
+import matplotlib.pyplot as plt
+import scipy.fftpack
 
 from key import Key
 
 class Wav_Manager():
-	def __init__(self, fs=128000, s=128000, a=100, t=10):
+	def __init__(self, fs=128000, s=128000, a=100, t=.1):
 		self.A = a
 		self.Fs = fs
 		self.sample = s
 		self.frequencies = None
-		self.x = np.arange(self.sample*t)
-		self.sum = np.zeros(self.sample*t)
+		self.x = np.arange(int(self.sample*t))
+		self.sum = np.zeros(int(self.sample*t))
 		self.y = None
 
 	def newWave(self, freq):
@@ -24,6 +27,58 @@ class Wav_Manager():
 		
 	def saveToFile(self, file='test.wav'):
 		wavfile.write(file, self.Fs, self.y)
+		print("* Wrote audio file!")
 		
 	def getFile(self, file='test.wav'):
 		return wavfile.read(file)
+		
+	def play(self):
+		print("* Generating sample...")
+		tone_out = np.array(self.y, dtype='int16')
+		bytestream = tone_out.tobytes()
+		pya = pyaudio.PyAudio()
+		stream = pya.open(format=pya.get_format_from_width(width=1), channels=1, rate=self.Fs, output=True)
+		try:
+			print ("* Previewing audio... Press 'Ctrl + c' to stop playback")
+			while True:
+				stream.write(bytestream)
+		except KeyboardInterrupt:
+			pass
+		stream.stop_stream()
+		stream.close()
+
+		pya.terminate()
+		print("* Preview completed!")
+		
+	def listen(self):
+		CHUNK = 4096 # number of data points to read at a time
+		RATE = 44100 # time resolution of the recording device (Hz)
+
+		p = pyaudio.PyAudio() # start the PyAudio class
+		stream = p.open(format=pyaudio.paInt16,channels=1,rate=RATE,input=True,
+					  frames_per_buffer=CHUNK) #uses default input device
+					  
+		avg = np.zeros(CHUNK, dtype='int16')
+				
+		try:
+			while True:
+				data = np.fromstring(stream.read(CHUNK),dtype='int16')
+				avg = (avg + data) / 2
+				plt.pause(.00001)
+				plt.gcf().clear()
+				plt.plot(scipy.fftpack.fft(avg)[:1000])
+				plt.draw()
+		except KeyboardInterrupt:
+			pass
+
+		stream.stop_stream()
+		stream.close()
+		p.terminate()
+		
+		return avg
+		
+		
+		
+		
+		
+		
